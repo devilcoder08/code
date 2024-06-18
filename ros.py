e4a5c20e7260be3bf4130a10b9c7653055cd396a
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-06-18 22:24:37 +0530

    fixed changfes

--- ros.py
+++ ros.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
@@ -6,15 +7,12 @@ from turtlesim.msg import Pose
 import math
 import time
 
-
 class XController(Node):
-    def _init_(self):
-        super()._init_("turtle_mover")
-        self.publisher_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
-        self.subscription = self.create_subscription(
-            Pose, "/turtle1/pose", self.pose_callback, 10
-        )
-        self.current_pose = Pose()
+    def __init__(self):
+        super().__init__('turtle_mover')
+        self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.subscription = self.create_subscription(Pose,'/turtle1/pose',self.pose_callback,10)
+        self.current_pose = None
         self.Kp = 0.5
         self.distance_threshold = 0.1
         self.linear_speed = 1.0
@@ -22,63 +20,125 @@ class XController(Node):
     def pose_callback(self, msg):
         self.current_pose = msg
 
-    def move_to_goal(self, goal_x, goal_y):
-        twist_msg = Twist()
-        self.distance = math.sqrt(
-            (goal_x - self.current_pose.x) ** 2 + (goal_y - self.current_pose.y) ** 2
-        )
-
-        while self.distance >= self.distance_threshold and rclpy.ok():
-            # maybe in this case it is not synchronised so the below might help
-            rclpy.spin_once(self)
-
-            error_x = goal_x - self.current_pose.x
-            error_y = goal_y - self.current_pose.y
-            self.distance = math.sqrt((error_x) ** 2 + (error_y) ** 2)
-            linear_vel = self.Kp * self.distance
-
-            twist_msg.linear.x = linear_vel * self.linear_speed
-            self.publisher_.publish(twist_msg)
-
-            time.sleep(0.1)
-
-        twist_msg.linear.x = 0.0
-        self.publisher_.publish(twist_msg)
-
     def run(self):
-        rclpy.spin_once(self)
-
-        self.move_to_goal(9.544445, 9.544445)
-        self.turn_turtle(0.7853)
-
-        self.move_to_goal(2.544445, 2.544445)
-        self.turn_turtle(3.14)
-
-        self.move_to_goal(5.544445, 5.544445)
-        self.turn_turtle(3.14)
-
-        self.move_to_goal(2.544445, 9.544445)
-        self.turn_turtle(1.57)
-
-        self.move_to_goal(9.544445, 2.544445)
-        self.turn_turtle(3.14)
-        self.destroy_node()
-        rclpy.shutdown()
-
-    def turn_turtle(self, angle):
         twist_msg = Twist()
-        twist_msg.angular.z = angle
+        twist_msg.angular.z = 0.7853
         self.publisher_.publish(twist_msg)
         time.sleep(1.0)
-        twist_msg.angular.z = 0.0
+        twist_msg = Twist()
+        self.cpx=5.544445
+        self.cpy=5.544445
+        self.tpx=9.544445
+        self.tpy=9.544445
+        self.distance = math.sqrt((self.tpx - self.cpx)**2 + (self.tpy - self.cpy)**2)
+        while True:
+            rclpy.spin_once(self)
+            error_x = self.tpx - self.cpx
+            error_y = self.tpy - self.cpy
+            linear_vel = self.Kp * math.sqrt(error_x**2 + error_y**2)
+            twist_msg.linear.x =  linear_vel * self.linear_speed
+            self.publisher_.publish(twist_msg)
+            time.sleep(1.0)
+            self.cpx=self.cpx+error_x
+            self.cpy=self.cpy+error_y
+            if linear_vel < self.distance_threshold:
+                break
+        twist_msg = Twist()
+        twist_msg.angular.z = 3.14
         self.publisher_.publish(twist_msg)
-
+        time.sleep(1.0)
+        twist_msg = Twist()
+        self.cpx=9.544445
+        self.cpy=9.544445
+        self.tpx=2.544445
+        self.tpy=2.544445
+        self.distance = math.sqrt((self.tpx - self.cpx)**2 + (self.tpy - self.cpy)**2)
+        while True:
+            rclpy.spin_once(self)
+            error_x = self.tpx - self.cpx
+            error_y = self.tpy - self.cpy
+            linear_vel = self.Kp * math.sqrt(error_x**2 + error_y**2)
+            twist_msg.linear.x =  linear_vel * self.linear_speed
+            self.publisher_.publish(twist_msg)
+            time.sleep(1.0)
+            self.cpx=self.cpx+error_x
+            self.cpy=self.cpy+error_y
+            if linear_vel < self.distance_threshold:
+                break
+        twist_msg = Twist()
+        twist_msg.angular.z = 3.14
+        self.publisher_.publish(twist_msg)
+        time.sleep(1.0)
+        twist_msg = Twist()
+        self.cpx=2.544445
+        self.cpy=2.544445
+        self.tpx=5.544445
+        self.tpy=5.544445
+        self.distance = math.sqrt((self.tpx - self.cpx)**2 + (self.tpy - self.cpy)**2)
+        while True:
+            rclpy.spin_once(self)
+            error_x = self.tpx - self.cpx
+            error_y = self.tpy - self.cpy
+            linear_vel = self.Kp * math.sqrt(error_x**2 + error_y**2)
+            twist_msg.linear.x =  linear_vel * self.linear_speed
+            self.publisher_.publish(twist_msg)
+            time.sleep(1.0)
+            self.cpx=self.cpx+error_x
+            self.cpy=self.cpy+error_y
+            if linear_vel < self.distance_threshold:
+                break
+        twist_msg = Twist()
+        twist_msg.angular.z = 1.57
+        self.publisher_.publish(twist_msg)
+        time.sleep(1.0)
+        twist_msg = Twist()
+        self.cpx=5.544445
+        self.cpy=5.544445
+        self.tpx=2.544445
+        self.tpy=9.544445
+        self.distance = math.sqrt((self.tpx - self.cpx)**2 + (self.tpy - self.cpy)**2)
+        while True:
+            rclpy.spin_once(self)
+            error_x = self.tpx - self.cpx
+            error_y = self.tpy - self.cpy
+            linear_vel = self.Kp * math.sqrt(error_x**2 + error_y**2)
+            twist_msg.linear.x =  linear_vel * self.linear_speed
+            self.publisher_.publish(twist_msg)
+            time.sleep(1.0)
+            self.cpx=self.cpx+error_x
+            self.cpy=self.cpy+error_y
+            if linear_vel < self.distance_threshold:
+                break
+        twist_msg = Twist()
+        twist_msg.angular.z = 3.14
+        self.publisher_.publish(twist_msg)
+        time.sleep(1.0)
+        twist_msg = Twist()
+        self.cpx=2.544445
+        self.cpy=9.544445
+        self.tpx=9.544445
+        self.tpy=2.544445
+        self.distance = math.sqrt((self.tpx - self.cpx)**2 + (self.tpy - self.cpy)**2)
+        while True:
+            rclpy.spin_once(self)
+            error_x = self.tpx - self.cpx
+            error_y = self.tpy - self.cpy
+            linear_vel = self.Kp * math.sqrt(error_x**2 + error_y**2)
+            twist_msg.linear.x =  linear_vel * self.linear_speed
+            self.publisher_.publish(twist_msg)
+            time.sleep(1.0)
+            self.cpx=self.cpx+error_x
+            self.cpy=self.cpy+error_y
+            if linear_vel < self.distance_threshold:
+                break
+        rclpy.spin_once(self)
+        self.destroy_node()
+        rclpy.shutdown()
 
 def main(args=None):
     rclpy.init(args=args)
     controller = XController()
     controller.run()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
