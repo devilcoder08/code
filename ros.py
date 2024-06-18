8b5906e3d17216d2da4c1ff9f691840858b1f148
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-06-18 21:09:35 +0530

    made sync changes

--- ros.py
+++ ros.py
@@ -6,11 +6,14 @@ from turtlesim.msg import Pose
 import math
 import time
 
+
 class XController(Node):
     def _init_(self):
-        super()._init_('turtle_mover')
-        self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.subscription = self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10)
+        super()._init_("turtle_mover")
+        self.publisher_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
+        self.subscription = self.create_subscription(
+            Pose, "/turtle1/pose", self.pose_callback, 10
+        )
         self.current_pose = Pose()
         self.Kp = 0.5
         self.distance_threshold = 0.1
@@ -21,12 +24,17 @@ class XController(Node):
 
     def move_to_goal(self, goal_x, goal_y):
         twist_msg = Twist()
-        self.distance = math.sqrt((goal_x - self.current_pose.x)**2 + (goal_y - self.current_pose.y)**2)
+        self.distance = math.sqrt(
+            (goal_x - self.current_pose.x) ** 2 + (goal_y - self.current_pose.y) ** 2
+        )
 
         while self.distance >= self.distance_threshold and rclpy.ok():
+            # maybe in this case it is not synchronised so the below might help
+            rclpy.spin_once(self)
+
             error_x = goal_x - self.current_pose.x
             error_y = goal_y - self.current_pose.y
-            self.distance = math.sqrt((error_x)**2 + (error_y)**2)
+            self.distance = math.sqrt((error_x) ** 2 + (error_y) ** 2)
             linear_vel = self.Kp * self.distance
 
             twist_msg.linear.x = linear_vel * self.linear_speed
@@ -39,20 +47,19 @@ class XController(Node):
 
     def run(self):
         rclpy.spin_once(self)
-        
-        
+
         self.move_to_goal(9.544445, 9.544445)
         self.turn_turtle(0.7853)
-        
+
         self.move_to_goal(2.544445, 2.544445)
         self.turn_turtle(3.14)
-        
+
         self.move_to_goal(5.544445, 5.544445)
         self.turn_turtle(3.14)
-        
+
         self.move_to_goal(2.544445, 9.544445)
         self.turn_turtle(1.57)
-        
+
         self.move_to_goal(9.544445, 2.544445)
         self.turn_turtle(3.14)
         self.destroy_node()
@@ -66,10 +73,12 @@ class XController(Node):
         twist_msg.angular.z = 0.0
         self.publisher_.publish(twist_msg)
 
+
 def main(args=None):
     rclpy.init(args=args)
     controller = XController()
     controller.run()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
